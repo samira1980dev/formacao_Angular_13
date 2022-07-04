0cc8fd336883587adbe261c32ee885f7cf41389e
Samira Marcela Roman <samira1980dev@gmail.com>
2022-07-04 19:11:46 -0300

    parte 2

--- jogo-da-velha/src/app/jogo-da-velha/shared/jogo-da-velha.service.ts
+++ jogo-da-velha/src/app/jogo-da-velha/shared/jogo-da-velha.service.ts
@@ -60,6 +60,45 @@ export class JogoDaVelhaService {
     this._showTabuleiro = true;
   }
 
+  jogar(posX: number, posY: number): void {
+    if (this.tabuleiro[posX][posY] !== this.VAZIO || this.vitoria) {
+      return;
+    }
+
+    this.tabuleiro[posX][posY] = this._jogador;
+    this.numMovimentos++;
+    this.vitoria = this.fimJogo(posX, posY, this.tabuleiro, this.jogador);
+    this._jogador = (this._jogador === this.X) ? this.O : this.X;
+
+    if (!this.vitoria && this.numMovimentos < 9) {
+      this.cpuJogar();
+    }
+
+    if (this.vitoria !== false) {
+      this._showFinal = true;
+    }
+
+    if(!this.vitoria && this.numMovimentos === 0) {
+      this._jogador = 0;
+      this._showFinal = true;
+    }
+  }
+
+  fimJogo(linha: number, coluna: number, tabuleiro: any, jogador: number) {
+    let fim: any = false;
+
+    if (tabuleiro[linha][0] === jogador &&
+      tabuleiro[linha][1] === jogador &&
+      tabuleiro[linha][2] === jogador) {
+        fim = [[linha, 0], [linha, 1], [linha, 2]];
+      }
+
+      if (tabuleiro)
+
+  }
+
+
+
   
 
 }
